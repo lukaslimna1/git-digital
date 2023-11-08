4c590c2c13bd81e46ca2b33904ddccea8f7d0a66
Lucas Mateus Soares de Lima <118951227+lukaslimna1@users.noreply.github.com>
2023-11-07 21:50:55 -0300

    Começando Java Script

---
+++ Java Script/primeiro.js
@@ -0,0 +1,3 @@
+//Primeira Aula de JS
+console.log('Hello, Word!');
+console.log('Bem-vindo ao cirso de JavaScript!');
---
+++ Java Script/variaveis.js
@@ -0,0 +1,25 @@
+// VARIÁVEIS
+
+let idade = 26 // Criando a primeira variavel
+
+console.log(idade);
+
+// Tipos de Variaveis
+
+let numero = 30 // Number
+let altura = 1.77 // Number
+let nome = 'Lucas' // String (Texto)
+let estudando = true  // Booleano (Verdadeiro ou Falso)
+
+console.log(numero, typeof numero);
+console.log(altura, typeof altura);
+console.log(nome, typeof nome);
+console.log(estudando, typeof estudando);
+
+let outra; // indefinida
+let nula = null  // Nula
+
+var variavel = 20 // não usa mais
+const vaca = 100 // Ele é uma constante e não pode ser mudada
+
+ 
